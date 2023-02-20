6ffa50cad846bbe2449510321c88e42ad6d8c021
NiPeGun <nipegun@gmail.com>
2023-02-20 20:19:06 +0100

    Create Preparar.sh

---
+++ PostInst/Preparar.sh
@@ -0,0 +1,14 @@
+pakfire update
+pakfire ugrade
+pakfire install -y lshw
+pakfire install -y mc
+pakfire install -y nmap
+pakfire install -y qemu-ga
+pakfire install -y rsync
+pakfire install -y samba
+pakfire install -y tcpdump
+pakfire install -y traceroute
+pakfire install -y transmission
+pakfire install -y watchdog
+pakfire install -y zabbix_agentd
+
